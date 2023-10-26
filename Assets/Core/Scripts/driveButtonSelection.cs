919046e93a436be68431c0c6cf20e66ffa265411
Liam <liam1148@live.co.uk>
2023-10-26 15:47:45 +0100

    Button now corresponds to the correct drive

---
+++ Assets/Core/Scripts/driveButtonSelection.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class driveButtonSelection : MonoBehaviour
+{
+    public void driveClicked()
+    {
+        Debug.Log(gameObject.name);
+    }
+}
